2426bd72d47ddbcaefb7c28f866f7109957a8720
HHjade21 <saehongburn4@gmail.com>
2022-03-27 22:54:55 +0900

    Please enter the commit message for your changes. Lines starting edit some codes

--- w4_homework_3.py
+++ w4_homework_3.py
@@ -2,7 +2,7 @@ import turtle
 import random
 
 ##전역 변수 선언 부분##
-myt, tX, tY, tcolor, tsize, tshape, tsum = [None] * 7
+myt, tX, tY, tcolor, tsize, tshape= [None] * 6
 turtleList = []
 
 ##메인 코드 부분##
@@ -17,20 +17,19 @@ if __name__ == "__main__" :
         tsize = random.randrange(10, 100)/10
         tX = random.randrange(-250, 250)
         tY = random.randrange(-200, 200)       
-        tSum = tX + tY
         
         r = random.random()
         g = random.random()
         b = random.random()
 
-        turtleList.append([myt, tsize, tX, tY, tsum, r, g, b])
+        turtleList.append([myt, tsize, tX, tY, r, g, b])
 
     sortedList = sorted(turtleList, key = lambda turtles : turtles[1], reverse = True)
 
     for index, tList in enumerate(sortedList[0:]):
         myt = tList[0]
-        myt.color((tList[5], tList[6], tList[7]))
-        myt.pencolor((tList[5], tList[6], tList[7]))
+        myt.color((tList[4], tList[5], tList[6]))
+        myt.pencolor((tList[4], tList[5], tList[6]))
         myt.turtlesize(tList[1])
         myt.penup()
         if index == 0:
